149e1d70c102217d6798c9bb8326620745c85a22
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-13 16:15:54 -0300

    create endpoint to display users page

--- src/main/java/com/tidz/sec2/controller/AppController.java
+++ src/main/java/com/tidz/sec2/controller/AppController.java
@@ -1,5 +1,7 @@
 package com.tidz.sec2.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -36,5 +38,12 @@ public class AppController {
 		this.userRepo.save(user);
 
 		return "register_success";
+	}
+
+	@GetMapping("/users")
+	public String listUsers(Model model) {
+		List<User> listUsers = this.userRepo.findAll();
+		model.addAttribute("listUsers", listUsers);
+		return "users";
 	}
 }
