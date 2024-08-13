d1d399429e72677cbc9e55984f8b99f4897d8923
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-13 12:24:44 -0300

    create endpoint to process user registration

--- src/main/java/com/tidz/sec2/controller/AppController.java
+++ src/main/java/com/tidz/sec2/controller/AppController.java
@@ -1,9 +1,11 @@
 package com.tidz.sec2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tidz.sec2.entity.User;
 import com.tidz.sec2.repository.UserRepository;
@@ -23,5 +25,16 @@ public class AppController {
 	public String showRegistrationForm(Model model) {
 		model.addAttribute("user", new User());
 		return "signup_form";
+	}
+
+	@PostMapping("/process_register")
+	public String processRegister(User user) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+
+		this.userRepo.save(user);
+
+		return "register_success";
 	}
 }
