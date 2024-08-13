93e966ca08f13b90ede89e04c21d8dae8efa89f4
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-13 12:09:49 -0300

    create signup page endpoint in the controller

--- src/main/java/com/tidz/sec2/controller/AppController.java
+++ src/main/java/com/tidz/sec2/controller/AppController.java
@@ -2,8 +2,10 @@ package com.tidz.sec2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.tidz.sec2.entity.User;
 import com.tidz.sec2.repository.UserRepository;
 
 @Controller
@@ -15,5 +17,11 @@ public class AppController {
 	@GetMapping("")
 	public String viewHomePage() {
 		return "index";
+	}
+
+	@GetMapping("/register")
+	public String showRegistrationForm(Model model) {
+		model.addAttribute("user", new User());
+		return "signup_form";
 	}
 }
